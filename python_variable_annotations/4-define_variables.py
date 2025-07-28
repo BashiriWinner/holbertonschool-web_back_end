9d2c02ec5c5be7dabe2d8c9b17b9fa53a10d3088
amil <abeshiri.ab@gmail.com>
2025-07-28 15:28:40 +0400

    fix: correct variable definitions and formatting in 4-define_variables.py

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
-def var_names(a: int = 1, pi: float = 3.14, i_understand_annotations: bool = True, school: str = "Holberton"):
-    return (a, pi, i_understand_annotations, school)
-)
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True 
+school: str = "Holberton"
+    
