84081b9c767405b7f84c93ce4ed82cfa04a89d2b
amil <abeshiri.ab@gmail.com>
2025-07-28 15:21:25 +0400

    feat: add var_names function to return variable names with default values

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+def var_names(a: int = 1, pi: float = 3.14, i_understand_annotations: bool = True, school: str = "Holberton"):
+    return (a, pi, i_understand_annotations, school)
+)
