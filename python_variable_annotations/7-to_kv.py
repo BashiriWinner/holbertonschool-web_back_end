2c1e5b7a91a05f673a50e6b2db222b2b0193e688
amil <abeshiri.ab@gmail.com>
2025-07-29 13:21:31 +0400

    fix: correct type hint for parameter v in to_kv function

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 from typing import Tuple, Union
 
-def to_kv(k: str, v: Tuple(Optional(int OR float))) -> float:
+def to_kv(k: str, v: Union[int OR float]) -> float:
     return(v, float(v * v))
