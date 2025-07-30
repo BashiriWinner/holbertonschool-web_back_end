af8c79edd082fe04ed99f22fe03cc830f28c3d1e
amil <abeshiri.ab@gmail.com>
2025-07-30 11:17:04 +0400

    fix: enhance docstring in add function to include parameter descriptions

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,4 +5,10 @@ This module provides a function to add two numbers.
 
 
 def add(a: float, b: float) -> float:
+    """
+    Keyword arguments:
+    a -- num1
+    b -- num2
+    Return: float
+    """
     return a + b
