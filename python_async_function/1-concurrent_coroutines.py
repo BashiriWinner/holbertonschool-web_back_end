7190ec96383c79426261455f102fe4eac886f137
amil <abeshiri.ab@gmail.com>
2025-07-30 15:45:29 +0400

    fix: clean up module docstring and update function return type in 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
-"""
-Module for concurrently waiting for random delays."""
-
-
-from typing import List
 import asyncio
+from typing import List
+
 from basic_async_syntax import wait_random
 
 
-
-async def wait_n(n: int, max_delay: int) -> float:
-    """
-    Asynchronously waits for n random delays and returns a list of the delays.
-    """
+async def wait_n(n: int, max_delay: int) -> List[float]:
     delays: List[float] = []
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     
