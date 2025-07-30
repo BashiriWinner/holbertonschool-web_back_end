8bec497c469acdef2c5195b87eeffaffe1a3b192
amil <abeshiri.ab@gmail.com>
2025-07-30 15:53:53 +0400

    fix: remove unnecessary newline in wait_n function in 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -14,7 +14,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     delays: List[float] = []
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
