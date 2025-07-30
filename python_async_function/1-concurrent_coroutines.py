df025f063191375a4b1bd17569803499daf73156
amil <abeshiri.ab@gmail.com>
2025-07-30 14:53:16 +0400

    feat: add 1-concurrent_coroutines.py with wait_n function for asynchronous delay handling

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python 3
+"""
+1-concurrent_coroutines.py
+"""
+
+
+import asyncio
+import wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """
+    Asynchronously waits for n random delays and returns a list of the delays.
+    """
+    delays: List[float] = []
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    
+    return delays
