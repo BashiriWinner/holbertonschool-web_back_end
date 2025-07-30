2b802da6e73555634eed7942b29ff4ae24443ed5
amil <abeshiri.ab@gmail.com>
2025-07-30 11:20:47 +0400

    fix: update module docstring for clarity and add function docstring

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to compute the floor of a number."""
+
+
 import math
 
 def floor(n: float) -> int:
+    """
+    Computes the floor of a number.
+    """
     return math.floor(n)
