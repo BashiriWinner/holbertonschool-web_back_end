7a632044413da6fd7be24624cf66ccf9cf4a242d
amil <abeshiri.ab@gmail.com>
2025-07-30 14:22:42 +0400

    fix: add module docstring to 0-basic_async_syntax.py for clarity

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
+"""
+0-basic_async_syntax.py
+"""
+
+
 import random
 import asyncio
 
+
 async def wait_random(max_delay: int = 10) -> float:
+    """
+    Asynchronously waits for a random delay between 0 and max_delay seconds.
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
