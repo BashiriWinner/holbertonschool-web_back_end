fe0357212f7e7bf36078e9738af00aa01f1cda3c
amil <abeshiri.ab@gmail.com>
2025-07-31 15:25:02 +0400

    fix: update import statement for task_wait_random in 4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,7 +4,7 @@
 
 import asyncio
 from typing import List
-task_wait_n = __import__('4-tasks').task_wait_n
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
