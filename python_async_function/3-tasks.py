9563d196d6c8611c97e77de5250f4989818c347b
amil <abeshiri.ab@gmail.com>
2025-07-31 14:24:20 +0400

    fix: update module docstring in 3-tasks.py for clarity

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-3-tasks.py
+Modules imported.
 """
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
