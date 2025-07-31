a1d9cceabef2d6ab52c4f363f3d0bd34e26928ae
amil <abeshiri.ab@gmail.com>
2025-07-31 15:28:39 +0400

    fix: rename function wait_n to task_wait_n for consistency

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -5,7 +5,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Asynchronously waits for n random delays and returns a list of the delays.
     """
