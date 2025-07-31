b5bf964a236b60522d1db83a28134ea46d56b658
amil <abeshiri.ab@gmail.com>
2025-07-31 15:13:43 +0400

    fix: reorder imports for consistency in 4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -5,6 +5,8 @@
 
 
 import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
