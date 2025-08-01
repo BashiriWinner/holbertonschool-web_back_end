163055aa9f136d2bb9d488b3560139b7deb1bfdd
amil <abeshiri.ab@gmail.com>
2025-08-01 17:15:50 +0400

    fix: add missing docstring to async_generator function for clarity

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""async_generator() function"""
 from typing import Generator
 import asyncio
 import random
 
 async def async_generator()-> Generator[float, None]:
+    """An asynchronous generator that yields 10 random float values"""
 
     for i in range(10):
         await asyncio.sleep(1)
