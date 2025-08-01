5ff61003177714e6598ba3f23f76c953280446ef
amil <abeshiri.ab@gmail.com>
2025-08-01 17:17:41 +0400

    fix: update type hint in async_generator function to include return type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """async_generator() function"""
+
+
 from typing import Generator
 import asyncio
 import random
 
-async def async_generator()-> Generator[float, None]:
+async def async_generator()-> Generator[float, None, None]:
     """An asynchronous generator that yields 10 random float values"""
 
     for i in range(10):
