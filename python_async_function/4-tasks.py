d69c54f56f00a266805d48a09e004d8d3bbd7716
amil <abeshiri.ab@gmail.com>
2025-08-01 15:07:46 +0400

    fix: correct import statement for task_wait_random and set default parameters in task_wait_n

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -3,10 +3,10 @@
 import random
 import asyncio
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
+task_wait_random = __import__('3-tasks').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
     """
     Asynchronously waits for n random delays and returns a list of the delays.
     """
