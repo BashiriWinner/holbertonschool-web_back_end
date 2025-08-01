ca6a08ad90ca7906ab6c002bed509b62768fda73
amil <abeshiri.ab@gmail.com>
2025-08-01 19:37:09 +0400

    fix: remove unnecessary blank line in async_generator function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,6 +6,7 @@ from typing import Generator
 import asyncio
 import random
 
+
 async def async_generator()-> Generator[float, None, None]:
     """An asynchronous generator that yields 10 random float values"""
     for i in range(10):
