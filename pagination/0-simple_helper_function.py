5c8a13a8cdacecbac28584659533ea68c9b516f0
amil <abeshiri.ab@gmail.com>
2025-08-11 17:50:30 +0400

    fix: adjust formatting in index_range function docstring for clarity

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,8 +3,9 @@ def index_range(page, page_size):
     """Returns a tuple of start and end index for pagination."""
 
     start_index = (page - 1) * page_size
-    """End index is exclusive, so we multiply page by page_size."""
+    """End index is exclusive, so we multiply 
+    page by page_size."""
 
-    
+
     end_index = page * page_size
     return (start_index, end_index)
