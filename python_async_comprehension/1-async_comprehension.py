5db3b8a08a34a5870f70192572390d9eca608ed5
amil <abeshiri.ab@gmail.com>
2025-08-11 15:25:07 +0400

    fix: add missing newline before docstring in async_comprehension function

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,6 +3,7 @@ from typing import List
 from async_generator import async_generator  # Əvvəlki tapşırıqdan idxal
 
 async def async_comprehension() -> List[float]:
+
     """
     Collects 10 random numbers using async comprehension over async_generator.
     """
