6d6f61cbdc6fec0d7f73c210b39c4bc8093f1d5a
amil <abeshiri.ab@gmail.com>
2025-08-11 15:14:41 +0400

    feat: add async_comprehension function to collect random numbers

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import List
+from async_generator import async_generator  # Əvvəlki tapşırıqdan idxal
+
+async def async_comprehension() -> List[float]:
+    """
+    Collects 10 random numbers using async comprehension over async_generator.
+    
+    Returns:
+        List[float]: List of 10 random numbers.
+    """
+    return [num async for num in async_generator()]
