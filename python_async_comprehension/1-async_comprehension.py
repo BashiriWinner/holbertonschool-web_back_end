93189e704426ad562c49a52c65d940ac06aa14ab
amil <abeshiri.ab@gmail.com>
2025-08-14 14:37:36 +0400

    fix: update docstring format in async_comprehension function

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
+"""async_comprehension() function"""
+
+
 from typing import List
 from async_generator import async_generator
 
 async def async_comprehension() -> List[float]:
-
-    """
+    ""
     Collects 10 random numbers using async comprehension over async_generator.
     """
     
-
     Returns:
         List[float]: List of 10 random numbers.
     """
