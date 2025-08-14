ff8a431e32b51227c73089b172fac99792f93195
amil <abeshiri.ab@gmail.com>
2025-08-14 14:59:06 +0400

    fix: update docstring in async_comprehension function for clarity

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
 """async_comprehension() function"""
 
-
 from typing import List
 from async_generator import async_generator
 async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
-    ""
-    Collects 10 random numbers using async comprehension over async_generator.
-    """
+    """Collects 10 random float values from an asynchronous generator"""
     
-    Returns:
-        List[float]: List of 10 random numbers.
-    """
     return [num async for num in async_generator()]
-    """
