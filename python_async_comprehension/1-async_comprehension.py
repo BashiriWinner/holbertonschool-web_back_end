b6b039e3022965f4ac2b0c551b0bd8e2e998f160
amil <abeshiri.ab@gmail.com>
2025-08-14 15:04:10 +0400

    fix: remove unnecessary blank lines in async_comprehension.py for improved readability

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Collects 10 random float values from an asynchronous generator"""
-    
-    
     return [num async for num in async_generator()]
