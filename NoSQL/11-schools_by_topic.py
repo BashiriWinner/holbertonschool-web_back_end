61b5863e965b0afc0727636459f857f26881c99b
amil <abeshiri.ab@gmail.com>
2025-08-14 14:30:20 +0400

    fix: return list instead of cursor in schools_by_topic function

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,5 +4,8 @@
 from pymongo import MongoClient
 
 def schools_by_topic(mongo_collection, topic):
+    """Returns the list of school having a specific topic."""
+
+    
     result = mongo_collection.find({"topics": topic})
-    return result
+    return list(result)
