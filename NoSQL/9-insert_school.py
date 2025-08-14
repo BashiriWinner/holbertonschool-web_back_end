36c9a89ef631822a84a64a99901e1bbb02c0390d
amil <abeshiri.ab@gmail.com>
2025-08-14 12:37:30 +0400

    fix: correct comment syntax and improve code formatting in insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 // To insert a new document in a collection
+
+import pymongo
+
+
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new document in a collection."""
     
