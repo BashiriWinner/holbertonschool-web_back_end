a4ed6604f6ec9324cefe8d7eff190d853519fe70
amil <abeshiri.ab@gmail.com>
2025-08-14 13:09:27 +0400

    fix: improve docstring formatting in update_topics function

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,6 +5,9 @@
 from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
+    """Changes all topics of a school document based on the name."""
+
+    
     mongo_collection.update_one(
         {"name": name}, {"$set": {"topics": topics}}
     )
