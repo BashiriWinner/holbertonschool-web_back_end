61adfea2c86449ae15062dec0b5dfc946e3226d2
amil <abeshiri.ab@gmail.com>
2025-08-14 13:12:04 +0400

    fix: correct syntax for updating topics in update_topics function

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on the name."""
 
-    
+
     mongo_collection.update_one(
-        {"name": name}, {"$set": {"topics": topics}}
+        {"name": name}, {"$set": [{"topics": topics}]}
     )
