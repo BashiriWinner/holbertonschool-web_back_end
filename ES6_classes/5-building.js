f9f5b0387f2daf856ef50a2be791b3d9d94c9847
amil <abeshiri.ab@gmail.com>
2025-08-22 09:55:17 +0000

    5-building.js

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building { 
+	contructor(sqft) { 
+		this.sqft = sqft; 
+	}
+
+	get sqft() { 
+		return this._sqft; 
+	}
+
+if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+}
