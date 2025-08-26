c4d44cdd61794fde2735f062a433113a473a5641
amil <abeshiri.ab@gmail.com>
2025-08-26 08:31:18 +0000

    4-update_grade_by_city.js

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,5 @@
 export default function updateStudentGradeByCity(stundents, city, newGrades) { 
-	eturn students
+	return students
 	.filter(student => student.location === city)
 	.map(student => { 
 		const gradeObj = newGrades.find(grade => grade.studentId === student.id);
