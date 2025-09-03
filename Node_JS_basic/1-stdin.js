63c74b1f43a5d05fd99607b79b52a998e3306a31
amil <abeshiri.ab@gmail.com>
2025-09-03 11:07:59 +0000

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,11 @@
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.once('data', (data) => {
+  const name = data.trim();
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
+  process.stdin.pause(); // Axını dayandırırıq
   process.exit();
 });
