2d9b59a2e7f21535bb4616724cb4e56a93772bfe
amil <abeshiri.ab@gmail.com>
2025-09-05 08:51:53 +0000

    5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -8,25 +8,15 @@ const DB = process.argv[2];
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    // Write the header line
     res.write('This is the list of our students\n');
-    countStudents(DB)
-      .then((result) => {
-        // Join the result array with newlines and ensure no trailing newline
-        const response = result.join('\n');
-        res.end(response);
-      })
-      .catch((error) => {
-        res.end(`This is the list of our students\n${error.message}`);
-      });
-  } else {
-    // Handle invalid routes
-    res.statusCode = 404;
-    res.end('Not found');
+    countStudents(DB).then((result) => {
+      res.end(result.join('\n'));
+    }).catch((error) => {
+      res.end(`${error.message}`);
+    });
   }
 });
 
